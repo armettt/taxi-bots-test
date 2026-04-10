dc1c15729c0486fc743c72a07c1fee48bf2c1871
armettt <matveys090512@gmail.com>
2026-04-10 17:40:57 +0300

    Update database.py

--- shared/database.py
+++ shared/database.py
@@ -1,24 +1,28 @@
 import asyncpg
-import asyncio
-import logging
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+DB_SCHEMA = os.getenv("DB_SCHEMA", "neondb")
 
-pool: asyncpg.pool.Pool | None = None
+pool: asyncpg.Pool | None = None
+
 
 async def init_db():
     global pool
-    pool = await asyncpg.create_pool(DATABASE_URL, min_size=1, max_size=10)
-    logging.info("PostgreSQL pool initialized ✅")
+    pool = await asyncpg.create_pool(DATABASE_URL)
+    async with pool.acquire() as conn:
+        await conn.execute(f"SET search_path TO {DB_SCHEMA}")
+
 
 async def fetch(query, *args):
     async with pool.acquire() as conn:
         return await conn.fetch(query, *args)
 
+
 async def fetchrow(query, *args):
     async with pool.acquire() as conn:
         return await conn.fetchrow(query, *args)
+
 
 async def execute(query, *args):
     async with pool.acquire() as conn:
